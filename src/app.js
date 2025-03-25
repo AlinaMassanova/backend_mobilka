c97a436ecdcdecf8a36e0a9c906c26128c4f0797
rghoth <drkdphoktk>
2025-03-25 23:27:45 +0600

    Update app.js to register routes and middleware

--- src/app.js
+++ src/app.js
@@ -5,15 +5,16 @@ const authRoutes = require('./routes/authRoutes');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
+const userRoutes = require('./routes/userRoutes');
+const testRoutes = require('./routes/testRoutes');
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/auth', authRoutes);
-
+app.use('/user', userRoutes);
+app.use('/', testRoutes);
 app.use(errorHandler);
 
-const PORT = 3001;
-app.listen(PORT, '0.0.0.0', () => console.log(`Server is running on ${PORT}`));
-
 module.exports = app;
+app.listen(3001, '0.0.0.0', () => console.log('🚀 Сервер запущен на порту 3001'));
