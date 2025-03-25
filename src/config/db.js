8c06ad8f3bedb2a54468dff0ca75ed1ef489d7c1
rghoth <drkdphoktk>
2025-03-25 23:46:43 +0600

    Fix issues with joi installation, console log, eslint formatting

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,9 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PORT:', process.env.DB_PORT);
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
