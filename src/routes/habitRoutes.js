d0aa7ad507da5499884c6c134dfac34467c1ad74
elnuraErkinbekova <elnuraerkinbekova8@gmail.com>
2025-03-29 13:53:52 +0600

    Set up habit routes

--- src/routes/habitRoutes.js
+++ src/routes/habitRoutes.js
@@ -1,28 +1,12 @@
 const express = require('express');
-const habitController = require('../controllers/habitController');
-const habitService = require('../services/habitService');
-const authMiddleware = require('../middlewares/authMiddleware'); 
+const HabitController = require('../controllers/habitController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
 const router = express.Router();
 
-router.post('/', authMiddleware, async (req, res) => {
-    console.log("User in request:", req.user);
-  
-    const { name, color, image } = req.body;
-  
-    if (!name || !color) {
-      return res.status(400).json({ error: 'Habit name and color are required' });
-    }
-  
-    try {
-      const habit = await habitService.createHabit(req.user.id, name, color, image);
-      return res.status(201).json(habit);
-    } catch (error) {
-      console.error('Error creating habit:', error);
-      return res.status(500).json({ error: 'Failed to create habit' });
-    }
-  });
-  
+router.post('/', authMiddleware, HabitController.create);
+router.get('/', authMiddleware, HabitController.getAll);
 
-router.post('/', habitController.createHabit);
+router.post('/:habitId/ticks/:date', authMiddleware, HabitController.toggleTick);
 
 module.exports = router;
